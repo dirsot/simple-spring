c128c3952faaeaf09b25a18869b66cd89ba29980
dirsot <dirsot@gmail.com>
2012-01-24 00:18:34 +0100

    start

---
+++ src/test/springapp/web/HelloControllerTests.java
@@ -0,0 +1,16 @@
+package test.springapp.web;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import springapp.web.HelloController;
+
+import junit.framework.TestCase;
+
+public class HelloControllerTests extends TestCase {
+
+    public void testHandleRequestView() throws Exception{		
+        HelloController controller = new HelloController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);		
+        assertEquals("hello.jsp", modelAndView.getViewName());
+    }
+}
